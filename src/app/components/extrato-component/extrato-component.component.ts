0f5868a11efa6b7375219b19d2067a148e7da845
Diego <diego.silva@codeoncloud.com>
2024-05-23 15:20:10 -0300

    implementação do extrato bancario

--- src/app/components/extrato-component/extrato-component.component.ts
+++ src/app/components/extrato-component/extrato-component.component.ts
@@ -2,24 +2,40 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ExtratoService } from '../../service/extrato-service';
 
+interface Transacao {
+  nome: string;
+  tipoConta: string;
+  numeroConta: string;
+  saldo: number;
+  transacao: string;
+  valor: number;
+  data: string;
+}
+
 @Component({
   selector: 'app-extrato-component',
   templateUrl: './extrato-component.component.html',
   styleUrl: './extrato-component.component.css'
 })
 export class ExtratoComponentComponent {
+
+  extratoList: Transacao[] = [];
+  showExtrato: boolean = false;
+
   constructor(private transferenciaService: ExtratoService, private router: Router) { }
 
   onSubmit(conta: string) {
-   this.transferenciaService.extrato(conta).subscribe(
-     response => {
-       console.log('Extrato successful');
-       this.router.navigate(['/home']);
-     },
-     error => {
-       console.error('Extrato failed', error);
-       this.router.navigate(['/home']);
-     }
-   );
- }
+    this.transferenciaService.extrato(conta).subscribe(
+      (response: Transacao[]) => {
+        console.log('Extrato successful');
+        this.extratoList = response;
+        this.showExtrato = true; 
+        alert('Extrato obtido com sucesso!');
+      },
+      error => {
+        console.error('Extrato failed', error);
+        this.router.navigate(['/home']);
+      }
+    );
+  }
 }
--- src/app/service/extrato-service.ts
+++ src/app/service/extrato-service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,9 @@ export class ExtratoService {
 
   constructor(private http: HttpClient) { }
 
-  extrato(conta: string) {
-    const body = {
-      conta: conta
-    };
-
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
-    });
-
-    return this.http.post('http://localhost:8080/transacao/extrato', body, { headers: headers });
+  extrato(conta: string): Observable<any[]> {
+    const body = { conta };
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any[]>('http://localhost:8080/transacao/extrato', body, { headers });
   }
 }
